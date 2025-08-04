afcaf946409e3e00263bb57056c92b813dbe3a83
raperdadakan <raperdadakan@gmail.com>
2025-08-04 12:44:08 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -35,4 +36,5 @@ def upload_icon():
         return jsonify({"status": "error", "message": response.text}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
